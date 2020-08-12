d71a4bf06ff3dec8d43440d4abe05c0aaa715656
Willian Campos <williancampos1995@gmail.com>
2020-08-11 23:03:45 -0300

    adding color changes.

--- src/pages/RepositoryDetailsPage/styles.ts
+++ src/pages/RepositoryDetailsPage/styles.ts
@@ -1,63 +1,62 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        backgroundColor: '#FFF',
-        borderColor: '#E6E6F0',
-        borderRadius: 8,
-        marginBottom: 16,
-        overflow: 'hidden',
-    },
+  container: {
+    height: "100%",
+    backgroundColor: "#FFF",
+    borderColor: "#E6E6F0",
+    borderRadius: 8,
+    marginBottom: 16,
+    overflow: "hidden",
+  },
 
-    repositoryContainer: {
-        flexDirection: 'column',
-    
-    },
+  repositoryContainer: {
+    flexDirection: "column",
+  },
 
-    repositoryName: {
-        alignContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-        fontFamily: 'Archivo_700Bold',
-        color: '#000',
-        fontSize: 24,
-    },
+  repositoryName: {
+    alignContent: "center",
+    alignItems: "center",
+    margin: 10,
+    fontFamily: "Archivo_700Bold",
+    color: "#000",
+    fontSize: 24,
+  },
 
-    profile: {
-        marginTop: -10,
-        marginLeft: -15,
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 24
-    },
+  profile: {
+    marginTop: -10,
+    marginLeft: -15,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 24,
+  },
 
-    avatar: {
-        width: 60,
-        height: 60,
-        borderRadius: 32,
-        marginRight: 5,
-        backgroundColor: '#EEE',
-    },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 32,
+    marginRight: 5,
+    backgroundColor: "#EEE",
+  },
 
-    repositoryOwnerContainer: {
-        marginLeft: 16,
-    },
+  repositoryOwnerContainer: {
+    marginLeft: 16,
+  },
 
-    repositoryOwner: {
-        fontFamily: 'Archivo_700Bold',
-        color: '#32264D',
-        fontSize: 20,
-    },
+  repositoryOwner: {
+    fontFamily: "Archivo_700Bold",
+    color: "#333",
+    fontSize: 22,
+  },
 
-    repositoryDescription: {
-        paddingTop: 15,
-        marginHorizontal: 15,
-        fontFamily: 'Archivo_400Regular',
-        fontSize: 16,
-        lineHeight: 24,
-        color: '#000',
-    }
+  repositoryDescription: {
+    paddingTop: 15,
+    marginHorizontal: 15,
+    fontFamily: "Archivo_400Regular",
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#000",
+  },
 });
 
 export default styles;
